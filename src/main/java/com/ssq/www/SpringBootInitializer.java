eed9178524ac44dd7a194daa13c0de3944f14760
ssq <m15005857079@163.com>
2019-04-10 14:53:36 +0800

    修改

---
+++ src/main/java/com/ssq/www/SpringBootInitializer.java
@@ -0,0 +1,21 @@
+package com.ssq.www;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+/**
+ * 来设置外部tomcat容器
+ * @author ssq
+ *下午1:41:54
+ */
+public class SpringBootInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		//将当前类的主类字节码导入
+		return builder.sources(HelloWorldApplocation.class);
+	}
+ 
+}
